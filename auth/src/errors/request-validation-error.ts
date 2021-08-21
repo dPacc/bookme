1749d8b6909ac56ddbf41cfd57c0fc996324b905
dPacc <themdk01@gmail.com>
2021-08-22 01:15:43 +0400

    add: custom error abstract class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,21 +1,22 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
-    statusCode = 400;
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
 
-    constructor(public errors: ValidationError[]) {
-        super();
+  constructor(public errors: ValidationError[]) {
+    super("Invalid request parameters");
 
-        // Only because we are extending a built in class
-        Object.setPrototypeOf(this, RequestValidationError.prototype);
-    }
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
 
-    serializeErrors() {
-        return this.errors.map(err => {
-            return {
-                field: err.param,
-                message: err.msg
-            };
-        });
-    }
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return {
+        field: err.param,
+        message: err.msg,
+      };
+    });
+  }
 }
