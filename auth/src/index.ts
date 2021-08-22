0d4a9f06ed0e9917d16a96203a1d8392cb79c8c4
dPacc <themdk01@gmail.com>
2021-08-22 18:40:59 +0400

    add: connect auth to mongo, add user schema

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -12,6 +13,17 @@ import { NotFoundError } from "./errors/not-found-error";
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+// Connect to the database
+mongoose
+  .connect("mongodb://auth-mongo-srv:27017/auth", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(`DB connection error - ${err}`));
 
 // Middlewares
 app.use(express.json());
@@ -31,5 +43,5 @@ app.use(errorHandler);
 
 // Run the server
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!!!`);
+  console.log(`Listening on port ${port}`);
 });
