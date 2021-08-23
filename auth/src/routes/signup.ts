6df761612047cf3d54f3a14b29dfbc6a921d738d
dPacc <themdk01@gmail.com>
2021-08-23 11:24:49 +0400

    add: signup user controller

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,32 +1,43 @@
 import express, { Request, Response } from "express";
+import { User } from "../models/user";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
-
 // Route
-router.get("/api/users/signup", [
-    body('email')
-    .isEmail()
-    .withMessage('Invalid email'),
-    body('password')
-    .trim()
-    .isLength({ min: 6, max: 20 })
-    .withMessage('Password must be between 6 and 20 characters')
-], (req: Request, res: Response) => {
+router.get(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Password must be between 6 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
+    // If there are any errors, return them
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    // Check if email already exists
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
+    if (existingUser) {
+      return res.send({});
+    }
 
-});
+    // Create new user and save to db
+    const user = User.build({ email, password });
+    await user.save();
+
+    return res.status(201).send(user);
+  }
+);
 
 export { router as signupRouter };
-
