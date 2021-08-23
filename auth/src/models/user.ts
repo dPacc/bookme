104ba4d9c3b223bbcececb3704fa0e89acb8b37a
dPacc <themdk01@gmail.com>
2021-08-23 10:56:43 +0400

    add: static type checking

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,24 @@
 import mongoose from "mongoose";
 
+// An interface that describes properties required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes properties that user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes properties that user document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +30,15 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+const usr = User.build({
+  email: "test@gmail.com",
+  password: "abvasdasd",
+});
 
 export { User };
