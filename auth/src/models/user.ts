9a3c6402bc64f55f4e89728ab38d2434338ee452
dPacc <themdk01@gmail.com>
2021-08-23 11:57:22 +0400

    mod: user model to intercept password mod and hash it

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../utils/password";
 
 // An interface that describes properties required to create a new user
 interface UserAttrs {
@@ -30,15 +31,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
 const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
-
-const usr = User.build({
-  email: "test@gmail.com",
-  password: "abvasdasd",
-});
 
 export { User };
