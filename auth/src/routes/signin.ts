95ebe0626af868a11b1c9c2c78ae868826a71396
dPacc <themdk01@gmail.com>
2021-08-27 14:21:30 +0400

    add: basic signup route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,14 +1,28 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
-
 // Route
-router.post("/api/users/signin", (req, res) => {
-    res.send({
-        user: "User"
-    });
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Please enter your password"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    // If there are any errors, return them
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signinRouter };
-
