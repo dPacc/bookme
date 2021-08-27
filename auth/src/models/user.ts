459b7b36ce8adaa998f45ee1c0dd4af99ddd25cc
dPacc <themdk01@gmail.com>
2021-08-27 13:38:43 +0400

    mod: user model to not return password using toJson

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -20,16 +20,28 @@ interface UserDoc extends mongoose.Document {
   updatedAt: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 userSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
