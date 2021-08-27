2b496cd155a1746f3a8707385beb216f7e2823d0
dPacc <themdk01@gmail.com>
2021-08-27 19:15:18 +0400

    add: check current user logged in route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,14 +1,22 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-
 // Route
 router.get("/api/users/currentuser", (req, res) => {
-    res.send({
-        user: "User"
-    });
+  // Check if jwt is present in session cookie
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  // Check if the jwt is valid
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
-
