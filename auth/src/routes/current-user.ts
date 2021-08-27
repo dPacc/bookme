c8306c44ff4e12a2020def8cf5bf094d0d49fb86
dPacc <themdk01@gmail.com>
2021-08-27 23:56:35 +0400

    mod: move current user check to its own middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,22 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 
+import { currentUser } from "../middlewares/current-user";
+
 const router = express.Router();
 
 // Route
 router.get("/api/users/currentuser", (req, res) => {
-  // Check if jwt is present in session cookie
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  // Check if the jwt is valid
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    return res.send({ currentUser: payload });
-  } catch (error) {
-    return res.send({ currentUser: null });
-  }
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
