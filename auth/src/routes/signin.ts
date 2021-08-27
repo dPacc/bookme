352821a68569be074869e9bb530545bc3d60e9c4
dPacc <themdk01@gmail.com>
2021-08-27 18:14:15 +0400

    fix: user signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
+import { Password } from "../utils/password";
+import { User } from "../models/user";
 import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -16,8 +20,40 @@ router.post(
       .withMessage("Please enter your password"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    // asd
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    // Find user from DB
+    const existingUser = await User.findOne({ email });
+
+    // If user not found, throw error
+    if (!existingUser) {
+      throw new BadRequestError("User not found");
+    }
+
+    // Compare passwords
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    // If passwords don't match, throw error
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid password");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    );
+
+    // Store JWT on session
+    req.session = {
+      jwt: userJwt,
+    };
+
+    return res.status(200).send(existingUser);
   }
 );
 
