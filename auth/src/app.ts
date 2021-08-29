6bc5f6785c130299fd17ad0d966422532a1acc63
dPacc <themdk01@gmail.com>
2021-08-29 18:21:42 +0400

    mod: split index to app to facilitate testing with jest

---
+++ auth/src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+// Create the app instance
+const app = express();
+
+// Check for JWT secret
+if (!process.env.JWT_KEY) {
+  throw new Error("JWT_KEY must be set");
+}
+
+// Middlewares
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+// Routes
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+// Error handler middleware
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,24 +1,7 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-// Create the app instance
-const app = express();
+import { app } from "./app";
 
 const port = process.env.PORT || 3000;
-
-// Check for JWT secret
-if (!process.env.JWT_KEY) {
-  throw new Error("JWT_KEY must be set");
-}
 
 // Connect to the database
 mongoose
@@ -30,29 +13,6 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(`DB connection error - ${err}`));
-
-// Middlewares
-app.set("trust proxy", true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-// Routes
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", () => {
-  throw new NotFoundError();
-});
-
-// Error handler middleware
-app.use(errorHandler);
 
 // Run the server
 app.listen(port, () => {
