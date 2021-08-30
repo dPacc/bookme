5312a01d129b73d16c0e98c551d2c90315e11017
dPacc <themdk01@gmail.com>
2021-08-30 10:53:13 +0400

    add: user signup helper function

---
+++ auth/src/utils/signup.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+import { app } from "../app";
+
+export const signup = async () => {
+  const email = "test@gmail.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
