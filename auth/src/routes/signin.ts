4c7786349a0bf57e2ddcbd851375e56c4d65eb97
dPacc <themdk01@gmail.com>
2021-09-18 13:31:37 +0400

    mod: install common from @netraga/common module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,7 @@ import jwt from "jsonwebtoken";
 
 import { Password } from "../utils/password";
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@netraga/common";
 
 const router = express.Router();
 
