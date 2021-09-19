69cfa902c2275429d616e58199424907c6e349ca
dPacc <themdk01@gmail.com>
2021-09-19 10:35:02 +0400

    fix: global declaration for signin

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,5 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
+
+declare global {
+  var signin: () => string[];
+}
 
 let mongo: any;
 
@@ -33,3 +40,26 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+// Emulate sign in
+global.signin = () => {
+  // Build a JWT payload. {id, email}
+  const payload = {
+    id: "qewq233",
+    email: "test@test.com",
+  };
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object (Take generate JWT and assign it to an obj with key "jwt")/ {jwt: MY_JWT}
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // Return a string thats the cookie with the encoded data
+  return [`express:sess=${base64}`];
+};
