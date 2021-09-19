df2b3abace6e7de1188d0bc5ad3b6fb146f91706
dPacc <themdk01@gmail.com>
2021-09-19 10:35:15 +0400

    fix: global declaration for signin

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,7 +2,9 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
-  return request(app).post("/api/tickets").send({}).expect(200);
+  const response = await request(app).post("/api/tickets").send({});
+
+  expect(response.status).not.toEqual(404);
 });
 
 it("can only be accessed if the user is signed in", async () => {
@@ -10,7 +12,10 @@ it("can only be accessed if the user is signed in", async () => {
 });
 
 it("returns a status other than 401 if the user is signed in", async () => {
-  const response = await request(app).post("/api/tickets").send({});
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({});
 
   expect(response.status).not.toEqual(401);
 });
