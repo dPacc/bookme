21641c336dbcc041558f9ffe31cfe0d6642d36ec
dPacc <themdk01@gmail.com>
2021-09-19 11:16:28 +0400

    create: ticket model and complete the create ticket test

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+// An interface that describes properties required to create a new ticket
+interface TicketAttrs {
+  title: string;
+  price: number;
+  useeId: string;
+}
+
+// An interface that describes properties that ticket document has
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+// An interface that describes properties that ticket model has
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,16 +1,20 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
+// Test if route exists
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
 
   expect(response.status).not.toEqual(404);
 });
 
+// Test authorized user
 it("can only be accessed if the user is signed in", async () => {
   await request(app).post("/api/tickets").send({}).expect(401);
 });
 
+// Test other status code if user signed in
 it("returns a status other than 401 if the user is signed in", async () => {
   const response = await request(app)
     .post("/api/tickets")
@@ -20,6 +24,7 @@ it("returns a status other than 401 if the user is signed in", async () => {
   expect(response.status).not.toEqual(401);
 });
 
+// Test title in ticket
 it("returns an error if an invalid title is provided", async () => {
   // Send empty title
   await request(app)
@@ -42,6 +47,7 @@ it("returns an error if an invalid title is provided", async () => {
     .expect(400);
 });
 
+// Test price in ticket
 it("returns an error if an invalid price is provided", async () => {
   // Send negative price
   await request(app)
@@ -63,15 +69,27 @@ it("returns an error if an invalid price is provided", async () => {
     .expect(400);
 });
 
+// Test new ticket create
 it("creates a ticket with valid inputs", async () => {
-  // TODO: Add in a check to make sure the ticket was saved
+  let tickets = await Ticket.find({});
+
+  // There should be no tickets initially in the collection
+  expect(tickets.length).toEqual(0);
+
+  const title = "wutt";
 
   await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
-      title: "wutt",
+      title,
       price: 45,
     })
-    .expect(200);
+    .expect(201);
+
+  // One ticket should now be present in the collection
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(45);
 });
