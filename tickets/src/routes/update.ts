c857f46296d9896a3b83174f52b2c6e79314b0d3
dPacc <themdk01@gmail.com>
2021-09-20 20:35:40 +0400

    added: event update publisher, exec when ticket updated

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
 } from "@netraga/common";
 import { Ticket } from "../models/ticket";
 import { body } from "express-validator";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -37,6 +39,14 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    // Publish the update event to NATS
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
