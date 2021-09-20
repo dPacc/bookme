eba8ca0140aafd2370a837aa9355880b60607399
dPacc <themdk01@gmail.com>
2021-09-20 11:08:40 +0400

    added: abstract and base listener classes

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,16 @@
+import { Message } from "node-nats-streaming";
+import { Listener } from "./base-listener";
+
+export class TicketCreatedListener extends Listener {
+  subject = "ticket:created";
+  queueGroupName = "payments-service";
+
+  onMessage(data: any, msg: Message) {
+    console.log("Event data!", data);
+
+    console.log(data.name);
+    console.log(data.cost);
+
+    msg.ack();
+  }
+}
