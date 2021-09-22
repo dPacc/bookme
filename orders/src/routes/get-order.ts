14525ef32922b549a1cf8f2fa88c8fa4e36b369e
dPacc <themdk01@gmail.com>
2021-09-22 13:57:28 +0400

    add: get order by id route and tests

--- orders/src/routes/get-order.ts
+++ orders/src/routes/get-order.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@netraga/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
 // Get an order by ID
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
 
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 export { router as getOrderRouter };
--- orders/src/routes/get-orders.ts
+++ orders/src/routes/get-orders.ts
@@ -5,7 +5,7 @@ import { Order } from "../models/order";
 const router = express.Router();
 
 // Get all orders
-router.get("/api/orders", async (req: Request, res: Response) => {
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
   const orders = await Order.find({
     userId: req.currentUser!.id,
   }).populate("ticket");
