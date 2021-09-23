fcf6002fc4443c53ca587ee388b13bf9fe7623ce
dPacc <themdk01@gmail.com>
2021-09-23 15:09:18 +0400

    test: order create and cancelled event publish test

--- orders/src/routes/__test__/create-order.test.ts
+++ orders/src/routes/__test__/create-order.test.ts
@@ -4,6 +4,7 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import { Ticket } from "../../models/ticket";
 import { OrderStatus } from "@netraga/common";
+import { natsWrapper } from "../../nats-wrapper";
 
 // Test if ticket doesn't exist
 it("returns an error if the ticket does not exist", async () => {
@@ -61,4 +62,20 @@ it("reserves a ticket", async () => {
 });
 
 // Test order created event
-it.todo("emits an order created event");
+it("emits an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 100,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- orders/src/routes/__test__/delete-order.test.ts
+++ orders/src/routes/__test__/delete-order.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 // Test if order can be created and deleted(cancelled status)
 it("marks an order as cancelled", async () => {
@@ -36,4 +37,30 @@ it("marks an order as cancelled", async () => {
 });
 
 // Test
-it.todo("Emits an order cancelled event");
+it("Emits an order cancelled event", async () => {
+  // Create a ticket with the ticket model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  const user = global.signin();
+
+  // Make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
