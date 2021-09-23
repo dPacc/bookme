1de0d5911a788d2bad07e4cef249f70072a34eab
dPacc <themdk01@gmail.com>
2021-09-23 11:12:17 +0400

    add: order create and cancelled event publish

--- orders/src/routes/create-order.ts
+++ orders/src/routes/create-order.ts
@@ -11,6 +11,10 @@ import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
 import { Order } from "../models/order";
 
+// Events
+import { OrderCreatedPublisher } from "../events/publishers/order-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
+
 const router = express.Router();
 
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
@@ -59,6 +63,16 @@ router.post(
     await order.save();
 
     // Publish an event saying "order was created"
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId.toString(),
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+    });
 
     res.status(201).send(order);
   }
--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -7,6 +7,10 @@ import {
 } from "@netraga/common";
 import { Order } from "../models/order";
 
+// Events
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
+
 const router = express.Router();
 
 // Delete an order
@@ -27,7 +31,13 @@ router.delete(
     order.status = OrderStatus.Cancelled;
     await order.save();
 
-    // Publish an event saying this was cancelled
+    // Publish an event saying this was "cancelled"
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
