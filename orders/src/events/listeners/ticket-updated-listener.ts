418d27dd73f5ab47d36fb518468f46a73dc0f0aa
dPacc <themdk01@gmail.com>
2021-09-24 11:43:15 +0400

    add: ticket updated listener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,30 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketUpdatedEvent } from "@netraga/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketCreatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    // If ticket is not found, throw an error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // Update ticket
+    ticket.set({
+      title: data.title,
+      price: data.price,
+    });
+
+    // Save ticket
+    await ticket.save();
+
+    // Acknowledge message
+    msg.ack();
+  }
+}
