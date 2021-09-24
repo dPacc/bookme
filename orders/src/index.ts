b1faefb07d30e6d24dbd95618bd74c7f15512dbc
dPacc <themdk01@gmail.com>
2021-09-24 22:52:13 +0400

    fix: ticket created event on orders

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,67 +6,63 @@ import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listene
 
 const port = process.env.PORT || 3000;
 
-// Check for JWT secret
-if (!process.env.JWT_KEY) {
-  throw new Error("JWT_KEY must be set");
-}
+const start = async () => {
+  // Check for JWT secret
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be set");
+  }
 
-// Check for orders MongoDB URI
-if (!process.env.MONGO_URI) {
-  throw new Error("ORDERS MONGO URI NOT SET!");
-}
+  // Check for orders MongoDB URI
+  if (!process.env.MONGO_URI) {
+    throw new Error("ORDERS MONGO URI NOT SET!");
+  }
 
-// Check for orders NATS Cluster ID
-if (!process.env.NATS_CLUSTER_ID) {
-  throw new Error("ORDERS MONGO URI NOT SET!");
-}
+  // Check for orders NATS Cluster ID
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("ORDERS MONGO URI NOT SET!");
+  }
 
-// Check for orders NATS Client ID
-if (!process.env.NATS_CLIENT_ID) {
-  throw new Error("ORDERS MONGO URI NOT SET!");
-}
+  // Check for orders NATS Client ID
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("ORDERS MONGO URI NOT SET!");
+  }
 
-// Check for orders NATS URL
-if (!process.env.NATS_URL) {
-  throw new Error("ORDERS MONGO URI NOT SET!");
-}
+  // Check for orders NATS URL
+  if (!process.env.NATS_URL) {
+    throw new Error("ORDERS MONGO URI NOT SET!");
+  }
 
-// Connect to NATS
-natsWrapper.connect(
-  process.env.NATS_CLUSTER_ID,
-  process.env.NATS_CLIENT_ID,
-  process.env.NATS_URL
-);
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
 
-natsWrapper.client.on("close", () => {
-  console.log("[Orders] NATS connection closed");
-  process.exit();
-});
+    natsWrapper.client.on("close", () => {
+      console.log("[Orders] NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
 
-// Listen to Ticket created and updated events
-new TicketCreatedListener(natsWrapper.client).listen();
-new TicketUpdatedListener(natsWrapper.client).listen();
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
-process.on("SIGINT", () => {
-  natsWrapper.client.close();
-});
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("[Orders] Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
 
-process.on("SIGTERM", () => {
-  natsWrapper.client.close();
-});
+  // Run the server
+  app.listen(port, () => {
+    console.log(`[Orders] Listening on port ${port}`);
+  });
+};
 
-// Connect to the database
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("[Orders] Connected to MongoDB"))
-  .catch((err) => console.log(`[Orders] DB connection error - ${err}`));
-
-// Run the server
-app.listen(port, () => {
-  console.log(`[Orders] Listening on port ${port}`);
-});
+start();
