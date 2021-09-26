1acf4eed70ca167e78c81e9c4fa80b5111c5dae3
dPacc <themdk01@gmail.com>
2021-09-26 17:05:06 +0400

    add: order created listener tests

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import { Ticket } from "../../../models/ticket";
+import { OrderCreatedEvent, OrderStatus } from "@netraga/common";
+import { OrderCreatedListener } from "../order-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+
+// Create a setup function
+const setup = async () => {
+  // Create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 99,
+    userId: "asdf",
+  });
+  await ticket.save();
+
+  // Create the fake data event
+  const data: OrderCreatedEvent["data"] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: "alskdfj",
+    expiresAt: "alskdjf",
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+// Test sets userId of the ticket
+it("sets the userId of the ticket", async () => {
+  // Create the listener
+  const { listener, ticket, data, msg } = await setup();
+
+  // Call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // Write assertions to make sure the ticket was updated
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+// Test acks the message
+it("acks the message", async () => {
+  // Create the listener
+  const { listener, ticket, data, msg } = await setup();
+
+  // Call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // Write assertions to make sure the ack function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
