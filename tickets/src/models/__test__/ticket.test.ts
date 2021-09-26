3ab64719142bbc7644765cc535a5a15134115ba6
dPacc <themdk01@gmail.com>
2021-09-26 11:55:13 +0400

    add: testing versioning

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,5 +1,6 @@
 import { Ticket } from "../ticket";
 
+// Test optimistic concurrency control
 it("implements optimistic concurrency control", async () => {
   // Create an instance of a ticket
   const ticket = Ticket.build({
@@ -30,4 +31,20 @@ it("implements optimistic concurrency control", async () => {
   }
 
   throw new Error("Should not reach this point");
+});
+
+// Test versioning
+it("increments the version number on multiple saves", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+    userId: "123",
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
 });
