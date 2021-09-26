74cecb72c1e12119e1c0120ee03b36f528b6d431
dPacc <themdk01@gmail.com>
2021-09-26 17:49:58 +0400

    add: ticket updated publish event

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,7 +1,8 @@
-import { Listener, OrderCreatedEvent, Subjects } from "@netraga/common";
 import { Message } from "node-nats-streaming";
-import { Ticket } from "../../models/ticket";
+import { Listener, OrderCreatedEvent, Subjects } from "@netraga/common";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -21,6 +22,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // Save the ticket
     await ticket.save();
+
+    // Publish ticket update event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
 
     // ack the message
     msg.ack();
