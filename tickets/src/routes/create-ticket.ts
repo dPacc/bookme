cf2d39eb2494d219633a404d3e1d4d25b45097d9
dPacc <themdk01@gmail.com>
2021-09-26 12:49:43 +0400

    mod: add version number to publish events

--- tickets/src/routes/create-ticket.ts
+++ tickets/src/routes/create-ticket.ts
@@ -31,6 +31,7 @@ router.post(
     // Publish the event to NATS
     await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
--- tickets/src/routes/update-ticket.ts
+++ tickets/src/routes/update-ticket.ts
@@ -43,6 +43,7 @@ router.put(
     // Publish the update event to NATS
     new TicketUpdatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
