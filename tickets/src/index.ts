308506a6d9744a6888cbc39a989111d7a38b670c
dPacc <themdk01@gmail.com>
2021-09-29 15:44:01 +0400

    fix: tickets natsError subject must be supplied

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,67 +6,72 @@ import { natsWrapper } from "./nats-wrapper";
 
 const port = process.env.PORT || 3000;
 
-// Check for JWT secret
-if (!process.env.JWT_KEY) {
-  throw new Error("JWT_KEY must be set");
-}
+const start = async () => {
+  // Check for JWT secret
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be set");
+  }
 
-// Check for tickets MongoDB URI
-if (!process.env.MONGO_URI) {
-  throw new Error("TICKETS MONGO URI NOT SET!");
-}
+  // Check for tickets MongoDB URI
+  if (!process.env.MONGO_URI) {
+    throw new Error("TICKETS MONGO URI NOT SET!");
+  }
 
-// Check for tickets NATS Cluster ID
-if (!process.env.NATS_CLUSTER_ID) {
-  throw new Error("TICKETS MONGO URI NOT SET!");
-}
+  // Check for tickets NATS Cluster ID
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("TICKETS MONGO URI NOT SET!");
+  }
 
-// Check for tickets NATS Client ID
-if (!process.env.NATS_CLIENT_ID) {
-  throw new Error("TICKETS MONGO URI NOT SET!");
-}
+  // Check for tickets NATS Client ID
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("TICKETS MONGO URI NOT SET!");
+  }
 
-// Check for tickets NATS URL
-if (!process.env.NATS_URL) {
-  throw new Error("TICKETS MONGO URI NOT SET!");
-}
+  // Check for tickets NATS URL
+  if (!process.env.NATS_URL) {
+    throw new Error("TICKETS MONGO URI NOT SET!");
+  }
 
-// Connect to NATS
-natsWrapper.connect(
-  process.env.NATS_CLUSTER_ID,
-  process.env.NATS_CLIENT_ID,
-  process.env.NATS_URL
-);
+  try {
+    // Connect to NATS
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
 
-natsWrapper.client.on("close", () => {
-  console.log("[Tickets] NATS connection closed");
-  process.exit();
-});
+    natsWrapper.client.on("close", () => {
+      console.log("[Tickets] NATS connection closed");
+      process.exit();
+    });
 
-process.on("SIGINT", () => {
-  natsWrapper.client.close();
-});
+    process.on("SIGINT", () => {
+      natsWrapper.client.close();
+    });
 
-process.on("SIGTERM", () => {
-  natsWrapper.client.close();
-});
+    process.on("SIGTERM", () => {
+      natsWrapper.client.close();
+    });
 
-// Order created and cancelled listeners
-new OrderCreatedListener(natsWrapper.client).listen();
-new OrderCancelledListener(natsWrapper.client).listen();
+    // Order created and cancelled listeners
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
-// Connect to the database
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("[Tickets] Connected to MongoDB"))
-  .catch((err) => console.log(`[Tickets] DB connection error - ${err}`));
+    // Connect to the database
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log("[Orders] Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
 
-// Run the server
-app.listen(port, () => {
-  console.log(`[Tickets] Listening on port ${port}`);
-});
+  // Run the server
+  app.listen(port, () => {
+    console.log(`[Tickets] Listening on port ${port}`);
+  });
+};
+
+start();
