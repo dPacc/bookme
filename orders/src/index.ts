a660eb1b31dfe32f2d126b8ecb7d954689243c2c
dPacc <themdk01@gmail.com>
2021-10-06 17:55:34 +0400

    add: order cancelled listener to the index file

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
 import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
+import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
 
 const port = process.env.PORT || 3000;
 
@@ -48,6 +49,9 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+
+    // Expiration complete listener
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
