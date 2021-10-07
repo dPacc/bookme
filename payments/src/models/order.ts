b6a90a6fbe1c8dfe455dbbd1c8f246ef22327586
dPacc <themdk01@gmail.com>
2021-10-07 21:17:37 +0400

    add: versioning to orders collection

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { OrderStatus } from "@netraga/common";
 
 interface OrderAttrs {
@@ -44,6 +45,10 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+// Update if current
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
